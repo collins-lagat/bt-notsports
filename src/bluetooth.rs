769143095212a870d743f04d3552eda7433ecb39
Collins Lagat <cklagat@gmail.com>
2025-07-21 15:22:32 +0300

    feat: toggle bluetooth

--- src/bluetooth.rs
+++ src/bluetooth.rs
@@ -1,12 +1,17 @@
 use std::{cmp::Ordering, time::Duration};
 
 use anyhow::Result;
-use bluer::{Address, Session};
+use bluer::{Adapter, Address, Session};
 use futures::{FutureExt, StreamExt, stream::FuturesUnordered};
 use log::error;
-use tokio::sync::mpsc::{Sender, channel};
+use tokio::{
+    process::Command,
+    sync::mpsc::{Sender, channel},
+};
 
 use crate::app::AppEvent;
+
+const STATE_CHANGED_FAILED_RETRY_MS: u64 = 5_000;
 
 #[derive(Debug)]
 pub enum Action {
@@ -110,8 +115,111 @@ pub struct BTState {
     pub devices: Vec<BTDevice>,
 }
 
+async fn toggle_bluetooth(adapter: &Adapter, on: bool) {
+    //FROM: https://github.com/pop-os/cosmic-applets/blob/c539b0628be7ea66feb3840fdca60c9e59bf3c75/cosmic-applet-bluetooth/src/bluetooth.rs#L678-L710
+    if let Err(e) = adapter.set_powered(!on).await {
+        error!(
+            "Failed to power {} bluetooth adapter. {e:?}",
+            if on { "off" } else { "on" },
+        );
+    }
+
+    // rfkill will be persisted after reboot
+    let name = adapter.name();
+    let device_id = Command::new("rfkill")
+        .arg("list")
+        .arg("-n")
+        .arg("--output")
+        .arg("ID,DEVICE")
+        .output()
+        .await
+        .ok()
+        .and_then(|o| {
+            // Output looks like this:
+            // 0 acer-wireless
+            // 1 acer-bluetooth
+            // 2 hci0
+            // 3 phy0
+            //
+            // The adapter names are the same as the device names on the second column.
+            // So we need to find the name of the deafault adapter on the second column and
+            // return the ID of the adapter.
+            let lines = String::from_utf8(o.stdout).ok()?;
+            lines.split("\n").find_map(|row| {
+                let (id, cname) = row.trim().split_once(" ")?;
+                (name == cname).then_some(id.to_string())
+            })
+        });
+
+    if let Some(id) = device_id {
+        if let Err(e) = Command::new("rfkill")
+            .arg(if on { "block" } else { "unblock" })
+            .arg(id)
+            .output()
+            .await
+        {
+            error!("Failed to set bluetooth state using rfkill. {e:?}");
+        };
+    }
+
+    tokio::time::sleep(Duration::from_millis(100)).await;
+}
+
+async fn toggle_device(state: &mut BTState, app_tx: &Sender<AppEvent>, device: BTDevice) {}
+
+async fn scan_for_devices(state: &mut BTState, app_tx: &Sender<AppEvent>) {}
+
 pub async fn init_bluetooth(app_tx: Sender<AppEvent>) -> Result<Sender<BTEvent>> {
     let (tx, mut rx) = channel::<BTEvent>(32);
+
+    // FROM: https://github.com/pop-os/cosmic-applets/blob/c171f048a6dff1a032eb5edf8f343cac60971ac5/cosmic-applet-bluetooth/src/bluetooth.rs#L82,L97
+    //
+    // ChatGPT says this code is attempting to establish a session with retry logic, using exponential backoff.
+    // - 2_u64 is just the literal integer (i32) 2, but instucts the compiler treat it as a u64.
+    //   saturating_pow is implemented for u64 and not i32. the rust compiler would assume the
+    //   integer was i32 if it was not explicitly specified.
+    // - u32 is the same as u64 but is a smaller integer type.
+    // - each iteration of the loop adds 1 to the retry_count, meaning the result of 2^retry_count
+    //   increases exponentially in each iteration.
+    // - 2^retry_count (exponential growth)
+    // - they use .saturating_pow(u32) instead of .pow(u32) to avoid overflowing. it returns the
+    //   max or min bound when the result is too large to fit in the return type.
+    // - 68_719_476_734 is the time in milliseconds. This is actially 2.18 years.
+    // - the original code chained `.max(68719476734)` on to the result of the pow call, but
+    //   ChatGPT (which I also agree after looking at the docs) says that it's possibly a
+    //   mistake/bug. `.max(68719476734)` compares the result of the pow call to 68719476734,
+    //   and returns the larger of the two. Meaning, on the first iteration, the result of the
+    //   pow call will be 2, which is less than 68719476734, so 68719476734 will be returned.
+    //   Therefore, the loop will wait for 2 years before retrying!
+    //   I have created a [PR](https://github.com/pop-os/cosmic-applets/issues/997) to fix this.
+    // - ChatGPT suggest that `.min(68719476734)` should be used instead of `.max(68719476734)`.
+    //   This will enable the exponential backoff to work correctly as it will exponentially
+    //   increase from 2 to 68719476734.
+
+    let mut retry_count = 0u32;
+
+    // Initialize connection.
+    let session = loop {
+        if let Ok(session) = Session::new().await {
+            break session;
+        }
+
+        // will run up to retry_count = 16 which 65,536 milliseconds which is roughly 1.1 seconds.
+        if retry_count >= 16 {
+            anyhow::bail!("Failed to connect to Bluetooth session");
+        }
+
+        retry_count = retry_count.saturating_add(1);
+        _ = tokio::time::sleep(Duration::from_millis(
+            2_u64.saturating_pow(retry_count).min(65_536),
+        ))
+        .await;
+    };
+
+    let adapter = session.default_adapter().await?;
+    let state = build_state(&adapter).await?;
+
+    tx.send(BTEvent::Init(state)).await?;
 
     tokio::spawn(async move {
         while let Some(action) = rx.recv().await {
@@ -121,69 +229,45 @@ pub async fn init_bluetooth(app_tx: Sender<AppEvent>) -> Result<Sender<BTEvent>>
                         error!("Failed to send BTState to AppEvent::Response: {e}");
                     };
                 }
-                BTEvent::Request { action, state } => todo!(),
+                BTEvent::Request { action, mut state } => {
+                    match action {
+                        Action::ToggleBluetooth => {
+                            toggle_bluetooth(&adapter, state.on).await;
+
+                            // There's a significant delay when turning off the adapter. Borrowing some ideas from GNOME's
+                            // bluetooth applet.
+                            // FROM: https://github.com/GNOME/gnome-shell/blob/4272916830120c0ff858e9b9de5d242a04932632/js/ui/status/bluetooth.js#L123-L140
+                            let app_tx = app_tx.clone();
+                            let local_adapter = adapter.clone();
+                            tokio::spawn(async move {
+                                tokio::time::sleep(Duration::from_millis(
+                                    STATE_CHANGED_FAILED_RETRY_MS,
+                                ))
+                                .await;
+
+                                if let Ok(state) = build_state(&local_adapter).await {
+                                    let _ = app_tx.send(AppEvent::Response(state)).await;
+                                }
+                            });
+                        }
+                        Action::ToggleDevice(device) => {
+                            toggle_device(&mut state, &app_tx, device).await
+                        }
+                        Action::Scan => scan_for_devices(&mut state, &app_tx).await,
+                    }
+
+                    if let Ok(state) = build_state(&adapter).await {
+                        let _ = app_tx.send(AppEvent::Response(state)).await;
+                    }
+                }
             }
         }
-    });
-
-    let local_tx = tx.clone();
-    tokio::spawn(async move {
-        // FROM: https://github.com/pop-os/cosmic-applets/blob/c171f048a6dff1a032eb5edf8f343cac60971ac5/cosmic-applet-bluetooth/src/bluetooth.rs#L82,L97
-        //
-        // ChatGPT says this code is attempting to establish a session with retry logic, using exponential backoff.
-        // - 2_u64 is just the literal integer (i32) 2, but instucts the compiler treat it as a u64.
-        //   saturating_pow is implemented for u64 and not i32. the rust compiler would assume the
-        //   integer was i32 if it was not explicitly specified.
-        // - u32 is the same as u64 but is a smaller integer type.
-        // - each iteration of the loop adds 1 to the retry_count, meaning the result of 2^retry_count
-        //   increases exponentially in each iteration.
-        // - 2^retry_count (exponential growth)
-        // - they use .saturating_pow(u32) instead of .pow(u32) to avoid overflowing. it returns the
-        //   max or min bound when the result is too large to fit in the return type.
-        // - 68_719_476_734 is the time in milliseconds. This is actially 2.18 years.
-        // - the original code chained `.max(68719476734)` on to the result of the pow call, but
-        //   ChatGPT (which I also agree after looking at the docs) says that it's possibly a
-        //   mistake/bug. `.max(68719476734)` compares the result of the pow call to 68719476734,
-        //   and returns the larger of the two. Meaning, on the first iteration, the result of the
-        //   pow call will be 2, which is less than 68719476734, so 68719476734 will be returned.
-        //   Therefore, the loop will wait for 2 years before retrying!
-        //   I have created a [PR](https://github.com/pop-os/cosmic-applets/issues/997) to fix this.
-        // - ChatGPT suggest that `.min(68719476734)` should be used instead of `.max(68719476734)`.
-        //   This will enable the exponential backoff to work correctly as it will exponentially
-        //   increase from 2 to 68719476734.
-
-        let mut retry_count = 0u32;
-
-        // Initialize connection.
-        let session = loop {
-            if let Ok(session) = Session::new().await {
-                break session;
-            }
-
-            // will run up to retry_count = 16 which 65,536 milliseconds which is roughly 1.1 seconds.
-            if retry_count >= 16 {
-                anyhow::bail!("Failed to connect to Bluetooth session");
-            }
-
-            retry_count = retry_count.saturating_add(1);
-            _ = tokio::time::sleep(Duration::from_millis(
-                2_u64.saturating_pow(retry_count).min(65_536),
-            ))
-            .await;
-        };
-
-        let state = build_state(&session).await?;
-
-        local_tx.send(BTEvent::Init(state)).await?;
-
-        Ok(())
     });
 
     Ok(tx)
 }
 
-async fn build_state(session: &Session) -> Result<BTState> {
-    let adapter = session.default_adapter().await?;
+async fn build_state(adapter: &Adapter) -> Result<BTState> {
     let on = adapter.is_powered().await?;
     let addresses = adapter.device_addresses().await.unwrap_or_default();
 
